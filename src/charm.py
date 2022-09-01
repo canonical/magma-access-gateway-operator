453c78589ed8bdc1fe441292d3949a505ea07c9b
Ghislain Bourgeois <ghislain.bourgeois@canonical.com>
2022-08-31 23:26:27 -0400

    Add configuration for all arguments of the snap installer

--- src/charm.py
+++ src/charm.py
@@ -4,12 +4,15 @@
 
 """Machine Charm for Magma's Access Gateway."""
 
+import ipaddress
 import logging
 import subprocess
+from typing import List
 
+import netifaces  # type: ignore[import]
 from ops.charm import CharmBase, InstallEvent
 from ops.main import main
-from ops.model import ActiveStatus
+from ops.model import ActiveStatus, BlockedStatus, MaintenanceStatus
 
 logger = logging.getLogger(__name__)
 
@@ -31,7 +34,13 @@ class MagmaAccessGatewayOperatorCharm(CharmBase):
         Returns:
             None
         """
+        self.unit.status = MaintenanceStatus("Installing AGW Snap")
         self.install_magma_access_gateway_snap()
+        if not self._is_configuration_valid():
+            self.unit.status = BlockedStatus("Configuration is invalid. Check logs for details")
+            event.defer()
+            return
+        self.unit.status = MaintenanceStatus("Installing AGW")
         self.install_magma_access_gateway()
         self.unit.status = ActiveStatus()
 
@@ -47,14 +56,18 @@ class MagmaAccessGatewayOperatorCharm(CharmBase):
             stdout=subprocess.PIPE,
         )
 
-    @staticmethod
-    def install_magma_access_gateway() -> None:
+    def install_magma_access_gateway(self) -> None:
         """Installs Magma access gateway on the host.
 
         Returns:
             None
         """
-        subprocess.run(["magma-access-gateway.install"], stdout=subprocess.PIPE)
+        command = ["magma-access-gateway.install"]
+        command.extend(self._install_arguments)
+        subprocess.run(
+            command,
+            stdout=subprocess.PIPE,
+        )
 
     @staticmethod
     def configure_magma_access_gateway(orc8r_domain: str, root_ca_path: str) -> None:
@@ -77,6 +90,112 @@ class MagmaAccessGatewayOperatorCharm(CharmBase):
             ],
             stdout=subprocess.PIPE,
         )
+
+    def _is_configuration_valid(self) -> bool:
+        """Validates configuration."""
+        valid = self._is_valid_interface("sgi", "eth0")
+        if not self._is_valid_interface("s1", "eth1"):
+            valid = False
+        if not self._is_valid_interface_addressing_configuration("sgi"):
+            valid = False
+        return valid
+
+    def _is_valid_interface(self, interface_name: str, new_interface_name: str) -> bool:
+        """Validates a network interface name."""
+        interface = self.model.config.get(interface_name)
+        if not interface:
+            logger.warning("%s interface name is required", (interface_name))
+            return False
+        if (
+            interface not in netifaces.interfaces()
+            and new_interface_name not in netifaces.interfaces()  # noqa: W503
+        ):
+            logger.warning("%s interface not found", (interface))
+            return False
+        return True
+
+    def _is_valid_interface_addressing_configuration(self, interface: str) -> bool:
+        """Validates sgi interface configuration."""
+        ipv4_address = self.model.config.get(f"{interface}-ipv4-address")
+        ipv4_gateway = self.model.config.get(f"{interface}-ipv4-gateway")
+        ipv6_address = self.model.config.get(f"{interface}-ipv6-address")
+        ipv6_gateway = self.model.config.get(f"{interface}-ipv6-gateway")
+        if not ipv4_address and not ipv4_gateway and not ipv6_address and not ipv6_gateway:
+            return True
+        if any([ipv4_address, ipv4_gateway]) and not all([ipv4_address, ipv4_gateway]):
+            logger.warning("Both IPv4 address and gateway required for interface %s", (interface))
+            return False
+        if any([ipv6_address, ipv6_gateway]) and not all([ipv6_address, ipv6_gateway]):
+            logger.warning("Both IPv6 address and gateway required for interface %s", (interface))
+            return False
+        if ipv6_address and not ipv4_address:
+            logger.warning(
+                "Pure IPv6 configuration is not supported for interface %s", (interface)
+            )
+            return False
+        if ipv4_address and not self._is_valid_ipv4_address(ipv4_address):
+            logger.warning("Invalid IPv4 address and netmask for interface %s", (interface))
+            return False
+        if ipv4_gateway and not self._is_valid_ipv4_gateway(ipv4_gateway):
+            logger.warning("Invalid IPv4 gateway for interface %s", (interface))
+            return False
+        if ipv6_address and not self._is_valid_ipv6_address(ipv6_address):
+            logger.warning("Invalid IPv6 address and netmask for interface %s", (interface))
+            return False
+        if ipv6_gateway and not self._is_valid_ipv6_gateway(ipv6_gateway):
+            logger.warning("Invalid IPv6 gateway for interface %s", (interface))
+            return False
+        return True
+
+    def _is_valid_ipv4_address(self, ipv4_address: str) -> bool:
+        """Validate an IPv4 address and netmask.
+
+        A valid string will have the form:
+        a.b.c.d/x
+        """
+        try:
+            ip = ipaddress.ip_network(ipv4_address, strict=False)
+            return isinstance(ip, ipaddress.IPv4Network)
+        except ValueError:
+            return False
+
+    def _is_valid_ipv4_gateway(self, ipv4_gateway: str) -> bool:
+        """Validate an IPv4 gateway.
+
+        A valid string will have the form:
+        a.b.c.d
+        """
+        try:
+            ip = ipaddress.ip_address(ipv4_gateway)
+            return isinstance(ip, ipaddress.IPv4Address)
+        except ValueError:
+            return False
+
+    def _is_valid_ipv6_address(self, ipv6_address: str) -> bool:
+        """Validate an IPv6 address and netmask."""
+        try:
+            ip = ipaddress.ip_network(ipv6_address, strict=False)
+            return isinstance(ip, ipaddress.IPv6Network)
+        except ValueError:
+            return False
+
+    def _is_valid_ipv6_gateway(self, ipv6_gateway: str) -> bool:
+        """Validate an IPv6 gateway."""
+        try:
+            ip = ipaddress.ip_address(ipv6_gateway)
+            return isinstance(ip, ipaddress.IPv6Address)
+        except ValueError:
+            return False
+
+    @property
+    def _install_arguments(self) -> List[str]:
+        """Prepares argument list for install command from configuration.
+
+        Returns:
+            List of arguments for install command
+        """
+        arguments = ["--sgi", self.model.config["sgi"], "--s1", self.model.config["s1"]]
+        return arguments
 
 
 if __name__ == "__main__":
--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -5,6 +5,7 @@ import unittest
 from unittest.mock import Mock, call, patch
 
 from ops import testing
+from ops.model import ActiveStatus, BlockedStatus
 
 from charm import MagmaAccessGatewayOperatorCharm
 
@@ -18,10 +19,313 @@ class TestMagmaAccessGatewayOperatorCharm(unittest.TestCase):
         self.harness.begin()
 
     @patch("subprocess.run")
-    def test_given_subprocess_doesnt_crash_when_install_then_snap_is_installed(
+    def test_given_no_config_provided_when_install_then_snap_is_installed(
         self, patch_subprocess_run
     ):
         event = Mock()
+        with self.assertLogs() as captured:
+            self.harness.charm._on_install(event=event)
+
+        patch_subprocess_run.assert_has_calls(
+            [
+                call(
+                    ["snap", "install", "magma-access-gateway", "--classic", "--edge"], stdout=-1
+                ),
+            ]
+        )
+        self.assertEqual(
+            self.harness.charm.unit.status,
+            BlockedStatus("Configuration is invalid. Check logs for details"),
+        )
+        self.assertEqual("sgi interface name is required", captured.records[0].getMessage())
+        self.assertEqual("s1 interface name is required", captured.records[1].getMessage())
+
+    @patch("subprocess.run")
+    @patch("netifaces.interfaces")
+    def test_given_invalid_interfaces_config_when_install_then_status_is_blocked(
+        self, patch_interfaces, patch_subprocess_run
+    ):
+        event = Mock()
+        patch_interfaces.return_value = ["enp0s1", "enp0s2"]
+        self.harness.update_config({"sgi": "nosuchinterface", "s1": "bananaphone"})
+        with self.assertLogs() as captured:
+            self.harness.charm._on_install(event=event)
+
+        self.assertEqual(
+            self.harness.charm.unit.status,
+            BlockedStatus("Configuration is invalid. Check logs for details"),
+        )
+        self.assertEqual("nosuchinterface interface not found", captured.records[0].getMessage())
+        self.assertEqual("bananaphone interface not found", captured.records[1].getMessage())
+
+    @patch("subprocess.run")
+    @patch("netifaces.interfaces")
+    def test_given_sgi_ipv4_address_and_no_gateway_in_config_when_install_then_status_is_blocked(
+        self,
+        patch_interfaces,
+        patch_subprocess_run,
+    ):
+        event = Mock()
+        patch_interfaces.return_value = ["enp0s1", "enp0s2"]
+        self.harness.update_config({"sgi": "enp0s1", "s1": "enp0s2"})
+        self.harness.update_config(
+            {
+                "sgi-ipv4-address": "10.0.0.2/24",
+            }
+        )
+        with self.assertLogs() as captured:
+            self.harness.charm._on_install(event=event)
+
+        self.assertEqual(
+            self.harness.charm.unit.status,
+            BlockedStatus("Configuration is invalid. Check logs for details"),
+        )
+        self.assertEqual(
+            "Both IPv4 address and gateway required for interface sgi",
+            captured.records[0].getMessage(),
+        )
+
+    @patch("subprocess.run")
+    @patch("netifaces.interfaces")
+    def test_given_sgi_ipv4_gateway_and_no_address_in_config_when_install_then_status_is_blocked(
+        self,
+        patch_interfaces,
+        patch_subprocess_run,
+    ):
+        event = Mock()
+        patch_interfaces.return_value = ["enp0s1", "enp0s2"]
+        self.harness.update_config({"sgi": "enp0s1", "s1": "enp0s2"})
+        self.harness.update_config(
+            {
+                "sgi-ipv4-gateway": "10.0.0.1",
+            }
+        )
+        with self.assertLogs() as captured:
+            self.harness.charm._on_install(event=event)
+
+        self.assertEqual(
+            self.harness.charm.unit.status,
+            BlockedStatus("Configuration is invalid. Check logs for details"),
+        )
+        self.assertEqual(
+            "Both IPv4 address and gateway required for interface sgi",
+            captured.records[0].getMessage(),
+        )
+
+    @patch("subprocess.run")
+    @patch("netifaces.interfaces")
+    def test_given_sgi_ipv6_address_and_no_gateway_in_config_when_install_then_status_is_blocked(
+        self,
+        patch_interfaces,
+        patch_subprocess_run,
+    ):
+        event = Mock()
+        patch_interfaces.return_value = ["enp0s1", "enp0s2"]
+        self.harness.update_config({"sgi": "enp0s1", "s1": "enp0s2"})
+        self.harness.update_config(
+            {
+                "sgi-ipv6-address": "2001:0db8:85a3:0000:0000:8a2e:0370:7334/64",
+            }
+        )
+        with self.assertLogs() as captured:
+            self.harness.charm._on_install(event=event)
+
+        self.assertEqual(
+            self.harness.charm.unit.status,
+            BlockedStatus("Configuration is invalid. Check logs for details"),
+        )
+        self.assertEqual(
+            "Both IPv6 address and gateway required for interface sgi",
+            captured.records[0].getMessage(),
+        )
+
+    @patch("subprocess.run")
+    @patch("netifaces.interfaces")
+    def test_given_sgi_ipv6_gateway_and_no_address_in_config_when_install_then_status_is_blocked(
+        self,
+        patch_interfaces,
+        patch_subprocess_run,
+    ):
+        event = Mock()
+        patch_interfaces.return_value = ["enp0s1", "enp0s2"]
+        self.harness.update_config({"sgi": "enp0s1", "s1": "enp0s2"})
+        self.harness.update_config(
+            {
+                "sgi-ipv6-gateway": "2001:0db8:85a3:0000:0000:8a2e:0370:7331",
+            }
+        )
+        with self.assertLogs() as captured:
+            self.harness.charm._on_install(event=event)
+
+        self.assertEqual(
+            self.harness.charm.unit.status,
+            BlockedStatus("Configuration is invalid. Check logs for details"),
+        )
+        self.assertEqual(
+            "Both IPv6 address and gateway required for interface sgi",
+            captured.records[0].getMessage(),
+        )
+
+    @patch("subprocess.run")
+    @patch("netifaces.interfaces")
+    def test_given_only_ipv6_sgi_config_when_install_then_status_is_blocked(
+        self,
+        patch_interfaces,
+        patch_subprocess_run,
+    ):
+        event = Mock()
+        patch_interfaces.return_value = ["enp0s1", "enp0s2"]
+        self.harness.update_config({"sgi": "enp0s1", "s1": "enp0s2"})
+        self.harness.update_config(
+            {
+                "sgi-ipv6-address": "2001:0db8:85a3:0000:0000:8a2e:0370:7334/64",
+                "sgi-ipv6-gateway": "2001:0db8:85a3:0000:0000:8a2e:0370:7331",
+            }
+        )
+        with self.assertLogs() as captured:
+            self.harness.charm._on_install(event=event)
+
+        self.assertEqual(
+            self.harness.charm.unit.status,
+            BlockedStatus("Configuration is invalid. Check logs for details"),
+        )
+        self.assertEqual(
+            "Pure IPv6 configuration is not supported for interface sgi",
+            captured.records[0].getMessage(),
+        )
+
+    @patch("subprocess.run")
+    @patch("netifaces.interfaces")
+    def test_given_invalid_sgi_ipv4_address_config_when_install_then_status_is_blocked(
+        self,
+        patch_interfaces,
+        patch_subprocess_run,
+    ):
+        event = Mock()
+        patch_interfaces.return_value = ["enp0s1", "enp0s2"]
+        self.harness.update_config({"sgi": "enp0s1", "s1": "enp0s2"})
+        self.harness.update_config(
+            {
+                "sgi-ipv4-address": "invalidip",
+                "sgi-ipv4-gateway": "10.0.0.1",
+            }
+        )
+        with self.assertLogs() as captured:
+            self.harness.charm._on_install(event=event)
+
+        self.assertEqual(
+            self.harness.charm.unit.status,
+            BlockedStatus("Configuration is invalid. Check logs for details"),
+        )
+        self.assertEqual(
+            "Invalid IPv4 address and netmask for interface sgi",
+            captured.records[0].getMessage(),
+        )
+
+    @patch("subprocess.run")
+    @patch("netifaces.interfaces")
+    def test_given_invalid_sgi_ipv4_gateway_config_when_install_then_status_is_blocked(
+        self,
+        patch_interfaces,
+        patch_subprocess_run,
+    ):
+        event = Mock()
+        patch_interfaces.return_value = ["enp0s1", "enp0s2"]
+        self.harness.update_config({"sgi": "enp0s1", "s1": "enp0s2"})
+        self.harness.update_config(
+            {
+                "sgi-ipv4-address": "10.0.0.2/24",
+                "sgi-ipv4-gateway": "not a gateway",
+            }
+        )
+        with self.assertLogs() as captured:
+            self.harness.charm._on_install(event=event)
+
+        self.assertEqual(
+            self.harness.charm.unit.status,
+            BlockedStatus("Configuration is invalid. Check logs for details"),
+        )
+        self.assertEqual(
+            "Invalid IPv4 gateway for interface sgi",
+            captured.records[0].getMessage(),
+        )
+
+    @patch("subprocess.run")
+    @patch("netifaces.interfaces")
+    def test_given_invalid_sgi_ipv6_address_config_when_install_then_status_is_blocked(
+        self,
+        patch_interfaces,
+        patch_subprocess_run,
+    ):
+        event = Mock()
+        patch_interfaces.return_value = ["enp0s1", "enp0s2"]
+        self.harness.update_config({"sgi": "enp0s1", "s1": "enp0s2"})
+        self.harness.update_config(
+            {
+                "sgi-ipv4-address": "10.0.0.2/24",
+                "sgi-ipv4-gateway": "10.0.0.1",
+            }
+        )
+        self.harness.update_config(
+            {
+                "sgi-ipv6-address": "not ipv6",
+                "sgi-ipv6-gateway": "2001:0db8:85a3:0000:0000:8a2e:0370:7331",
+            }
+        )
+        with self.assertLogs() as captured:
+            self.harness.charm._on_install(event=event)
+
+        self.assertEqual(
+            self.harness.charm.unit.status,
+            BlockedStatus("Configuration is invalid. Check logs for details"),
+        )
+        self.assertEqual(
+            "Invalid IPv6 address and netmask for interface sgi",
+            captured.records[0].getMessage(),
+        )
+
+    @patch("subprocess.run")
+    @patch("netifaces.interfaces")
+    def test_given_invalid_sgi_ipv6_gateway_config_when_install_then_status_is_blocked(
+        self,
+        patch_interfaces,
+        patch_subprocess_run,
+    ):
+        event = Mock()
+        patch_interfaces.return_value = ["enp0s1", "enp0s2"]
+        self.harness.update_config({"sgi": "enp0s1", "s1": "enp0s2"})
+        self.harness.update_config(
+            {
+                "sgi-ipv4-address": "10.0.0.2/24",
+                "sgi-ipv4-gateway": "10.0.0.1",
+            }
+        )
+        self.harness.update_config(
+            {
+                "sgi-ipv6-address": "2001:0db8:85a3:0000:0000:8a2e:0370:7334/64",
+                "sgi-ipv6-gateway": "not a gateway",
+            }
+        )
+        with self.assertLogs() as captured:
+            self.harness.charm._on_install(event=event)
+
+        self.assertEqual(
+            self.harness.charm.unit.status,
+            BlockedStatus("Configuration is invalid. Check logs for details"),
+        )
+        self.assertEqual(
+            "Invalid IPv6 gateway for interface sgi",
+            captured.records[0].getMessage(),
+        )
+
+    @patch("subprocess.run")
+    @patch("netifaces.interfaces")
+    def test_given_valid_dhcp_config_when_install_then_status_is_active(
+        self, patch_interfaces, patch_subprocess_run
+    ):
+        event = Mock()
+        patch_interfaces.return_value = ["enp0s1", "enp0s2"]
+        self.harness.update_config({"sgi": "enp0s1", "s1": "enp0s2"})
         self.harness.charm._on_install(event=event)
 
         patch_subprocess_run.assert_has_calls(
@@ -29,6 +333,51 @@ class TestMagmaAccessGatewayOperatorCharm(unittest.TestCase):
                 call(
                     ["snap", "install", "magma-access-gateway", "--classic", "--edge"], stdout=-1
                 ),
-                call(["magma-access-gateway.install"], stdout=-1),
+                call(
+                    ["magma-access-gateway.install", "--sgi", "enp0s1", "--s1", "enp0s2"],
+                    stdout=-1,
+                ),
             ]
+        )
+        self.assertEqual(
+            self.harness.charm.unit.status,
+            ActiveStatus(),
+        )
+
+    @patch("subprocess.run")
+    @patch("netifaces.interfaces")
+    def test_given_valid_static_config_when_install_then_status_is_active(
+        self, patch_interfaces, patch_subprocess_run
+    ):
+        event = Mock()
+        patch_interfaces.return_value = ["enp0s1", "enp0s2"]
+        self.harness.update_config({"sgi": "enp0s1", "s1": "enp0s2"})
+        self.harness.update_config(
+            {
+                "sgi-ipv4-address": "10.0.0.2/24",
+                "sgi-ipv4-gateway": "10.0.0.1",
+            }
+        )
+        self.harness.update_config(
+            {
+                "sgi-ipv6-address": "2001:0db8:85a3:0000:0000:8a2e:0370:7334/64",
+                "sgi-ipv6-gateway": "2001:0db8:85a3:0000:0000:8a2e:0370:7331",
+            }
+        )
+        self.harness.charm._on_install(event=event)
+
+        patch_subprocess_run.assert_has_calls(
+            [
+                call(
+                    ["snap", "install", "magma-access-gateway", "--classic", "--edge"], stdout=-1
+                ),
+                call(
+                    ["magma-access-gateway.install", "--sgi", "enp0s1", "--s1", "enp0s2"],
+                    stdout=-1,
+                ),
+            ]
+        )
+        self.assertEqual(
+            self.harness.charm.unit.status,
+            ActiveStatus(),
         )
