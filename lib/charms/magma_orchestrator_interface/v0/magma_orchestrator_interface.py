49c9e2c8edd5727d2c61a1653df46912220bdb58
Javier Sanchez <sanchezfdez.javier@gmail.com>
2022-11-18 18:52:49 +0100

    Fixes static analysis

--- lib/charms/magma_orchestrator_interface/v0/magma_orchestrator_interface.py
+++ lib/charms/magma_orchestrator_interface/v0/magma_orchestrator_interface.py
@@ -266,8 +266,8 @@ class OrchestratorRequires(Object):
         remote_app_relation_data = relation.data[relation.app]
         if not self._relation_data_is_valid(dict(remote_app_relation_data)):
             logger.warning(
-                f"Provider relation data did not pass JSON Schema validation: "
-                f"{event.relation.data[event.app]}"  # type: ignore[index]
+                f"Provider relation data did not pass JSON Schema validation: "  # type: ignore[index]
+                f"{event.relation.data[event.app]}"
             )
             return
         self.on.orchestrator_available.emit(
